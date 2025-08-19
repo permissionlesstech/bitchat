e484e99a0af090346857a16930d69eae447f2661
Wentao <s3956080@student.rmit.edu.au>
2025-08-19 11:27:09 +0800

    new views update

---
+++ bitchat/Views/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Wentao Guo on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bitchat/Views/ContentView 2.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Circle
+//
+//  Created by Wentao Guo on 14/08/25.
+//
+import SwiftUI
+
+// MARK: - Main App Entry
+struct ContentView: View {
+    init() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = UIColor(Color.brandOrange)
+
+     
+        tabBarAppearance.stackedLayoutAppearance.normal.iconColor = UIColor(
+            Color.white
+        ).withAlphaComponent(0.4)
+        tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor(Color.white).withAlphaComponent(0.4)
+        ]
+
+   
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = .white
+        tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes =
+            [
+                .foregroundColor: UIColor.white
+            ]
+
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        }
+    }
+
+    var body: some View {
+        TabView {
+            ChatsView()
+                .tabItem {
+                    Image("message-question")
+                        .renderingMode(.template)
+                    Text("Chats")
+                }
+            Text("Alerts")
+                .tabItem {
+                    Image("location")
+                        .renderingMode(.template)
+                    Text("location")
+                }
+            Text("Circles")
+                .tabItem {
+                    Image("person")
+                        .renderingMode(.template)
+                    Text("Person")
+                }
+            Text("Contacts")
+                .tabItem {
+                    Image("profile")
+                        .renderingMode(.template)
+                    Text("Profile")
+                }
+        }
+        .tint(.brandOrange)
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    ContentView()
+}
