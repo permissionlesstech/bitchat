48cea405bfdc93aa3a320999b6966ff7ea07f85e
islam <2553451+qalandarov@users.noreply.github.com>
2025-09-25 23:39:15 +0100

    Update all `NSLocalizedString` with `L10n.string`
    + combine with `L10n.format`

--- bitchat/Utils/Localization.swift
+++ bitchat/Utils/Localization.swift
@@ -1,12 +1,11 @@
 import Foundation
 
 enum L10n {
-    static func string(_ key: String, comment: String) -> String {
-        NSLocalizedString(key, comment: comment)
-    }
-
-    static func format(_ key: String, comment: String, _ args: CVarArg...) -> String {
-        let format = NSLocalizedString(key, comment: comment)
-        return String(format: format, locale: Locale.current, arguments: args)
+    static func string(_ key: String, comment: String, _ args: CVarArg...) -> String {
+        let basic = NSLocalizedString(key, comment: comment)
+        if args.isEmpty {
+            return basic
+        }
+        return String(format: basic, locale: .current, arguments: args)
     }
 }
