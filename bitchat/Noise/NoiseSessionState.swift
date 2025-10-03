44a0eedf078072aaf5a8b33206c6d79020b6758e
islam <2553451+qalandarov@users.noreply.github.com>
2025-10-03 23:30:47 +0100

    Remove `failed` state from `NoiseSessionState`

--- bitchat/Noise/NoiseSessionState.swift
+++ bitchat/Noise/NoiseSessionState.swift
@@ -10,18 +10,4 @@ enum NoiseSessionState: Equatable {
     case uninitialized
     case handshaking
     case established
-    case failed(Error)
-    
-    static func == (lhs: NoiseSessionState, rhs: NoiseSessionState) -> Bool {
-        switch (lhs, rhs) {
-        case (.uninitialized, .uninitialized),
-             (.handshaking, .handshaking),
-             (.established, .established):
-            return true
-        case (.failed, .failed):
-            return true // We don't compare the errors
-        default:
-            return false
-        }
-    }
 }
