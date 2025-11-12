545205bf6e40e009e061aedfaa32afaf78cd60ec
vekovius <vekovius@users.noreply.github.com>
2025-11-12 16:22:41 -0600

    Address PR feedback for utility docs and tests

--- bitchat/Utils/String+DJB2.swift
+++ bitchat/Utils/String+DJB2.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 extension String {
+    /// Returns a deterministic DJB2 hash for this string.
+    ///
+    /// DJB2 is a non-cryptographic hash used for stable peer colors and
+    /// other UI-related deterministic values. Do not use it for cryptographic
+    /// or security-sensitive purposes.
     func djb2() -> UInt64 {
         var hash: UInt64 = 5381
         for b in utf8 { hash = ((hash << 5) &+ hash) &+ UInt64(b) }
