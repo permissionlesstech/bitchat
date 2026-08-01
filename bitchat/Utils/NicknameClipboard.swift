35b06865c4f2e227715b6b7f8bf056c048a7e5c0
Taksh <takshkothari09@gmail.com>
2026-08-01 14:12:14 +0300

    feat: add NicknameClipboard helper for people-row copy
    One pasteboard write path for iOS and macOS so mesh and geohash menus
    do not duplicate UIKit/AppKit branches.

---
+++ bitchat/Utils/NicknameClipboard.swift
@@ -0,0 +1,26 @@
+//
+//  NicknameClipboard.swift
+//  bitchat
+//
+//  This is free and unencumbered software released into the public domain.
+//  For more information, see <https://unlicense.org>
+//
+
+import Foundation
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+/// Pasteboard writes for people-sheet nickname / display-name copy actions.
+enum NicknameClipboard {
+    static func copy(_ string: String) {
+        #if os(iOS)
+        UIPasteboard.general.string = string
+        #elseif os(macOS)
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(string, forType: .string)
+        #endif
+    }
+}
